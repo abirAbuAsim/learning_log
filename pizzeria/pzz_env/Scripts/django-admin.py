ca314bac4f3a91c83aede1160ca6f28fb0298a47
Abir Khan <abir.sakif@gmail.com>
2017-03-30 08:33:45 +0600

    change the arrangement of the directories

--- pizzeria/pzz_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\absak\dropbox\python\python~1\python~1\pizzeria\pzz_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
